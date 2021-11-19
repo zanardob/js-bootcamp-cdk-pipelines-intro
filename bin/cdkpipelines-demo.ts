ec1c34166cfb40bb488a74e766c76f92d3d93c9b
jarstingstall <jrarst01@gmail.com>
2021-11-19 10:19:31 -0500

    initialize cdk project and stacks

---
+++ bin/cdkpipelines-demo.ts
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+import { App } from "@aws-cdk/core";
+import { CdkpipelinesDemoPipelineStack } from "../lib/cdkpipelines-demo-pipeline-stack";
+
+const app = new App();
+
+new CdkpipelinesDemoPipelineStack(app, "CdkpipelinesDemoPipelineStack", {
+  env: { account: "ACCOUNT1", region: "us-east-2" },
+});
+
+app.synth();
