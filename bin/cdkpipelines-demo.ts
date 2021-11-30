14b8fd4c8af18f75637ae80eaecedb266952e919
Jeroen Rombouts <jrombouts@junglescout.com>
2021-11-30 20:21:59 +0100

    Remove pipeline stacks to prevent confusion in first lab

--- bin/cdkpipelines-demo.ts
+++ bin/cdkpipelines-demo.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import { App } from "@aws-cdk/core";
-import { CdkpipelinesDemoPipelineStack } from "../lib/cdkpipelines-demo-pipeline-stack";
+import { CdkpipelinesDemoStack } from "../lib/cdkpipelines-demo-stack";
 
 const app = new App();
 
-new CdkpipelinesDemoPipelineStack(app, "CdkpipelinesDemoPipelineStack", {
+new CdkpipelinesDemoStack(app, "CdkpipelinesDemoPipelineStack", {
   env: { account: "ACCOUNT1", region: "REGION" },
 });
 
