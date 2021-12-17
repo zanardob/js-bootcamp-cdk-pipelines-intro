853a5bb652ceff170f34b09b383b1c414d3d0f86
Guilherme Zanardo <guilherme@zanardo.dev>
2021-12-17 11:49:00 -0300

    Update GitHub repository name and AWS account ID

--- lib/cdkpipelines-demo-pipeline-stack.ts
+++ lib/cdkpipelines-demo-pipeline-stack.ts
@@ -12,14 +12,14 @@ export class CdkpipelinesDemoPipelineStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const pipeline = new CodePipeline(this, "Pipeline", {
+    new CodePipeline(this, "Pipeline", {
       // The pipeline name
       pipelineName: "MyServicePipeline",
 
       // How it will be built and synthesized
       synth: new ShellStep("Synth", {
         // Where the source can be found
-        input: CodePipelineSource.gitHub("OWNER/REPO", "main"),
+        input: CodePipelineSource.gitHub("zanardob/js-bootcamp-cdk-pipelines-intro", "main"),
 
         // Install dependencies, build and run cdk synth
         commands: ["npm ci", "npm run build", "npx cdk synth"],
