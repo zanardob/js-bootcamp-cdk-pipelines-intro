dccab6df527ed28a679fcd3521dc0ee4e23439bf
Guilherme Zanardo <guilherme@zanardo.dev>
2021-12-17 13:37:53 -0300

    Add production pipeline stage with manual approval

--- lib/cdkpipelines-demo-pipeline-stack.ts
+++ lib/cdkpipelines-demo-pipeline-stack.ts
@@ -3,8 +3,13 @@ import {
   CodePipeline,
   CodePipelineSource,
   ShellStep,
+  ManualApprovalStep
 } from "@aws-cdk/pipelines";
 import { CdkpipelinesDemoStage } from "./cdkpipelines-demo-stage";
+
+const account = "394535346065"
+const region = "us-west-2"
+const repo = "zanardob/js-bootcamp-cdk-pipelines-intro"
 
 /**
  * The stack that defines the application pipeline
@@ -20,7 +25,7 @@ export class CdkpipelinesDemoPipelineStack extends Stack {
       // How it will be built and synthesized
       synth: new ShellStep("Synth", {
         // Where the source can be found
-        input: CodePipelineSource.gitHub("zanardob/js-bootcamp-cdk-pipelines-intro", "main"),
+        input: CodePipelineSource.gitHub(repo, "main"),
 
         // Install dependencies, build and run cdk synth
         commands: ["npm ci", "npm run build", "npx cdk synth"],
@@ -30,8 +35,16 @@ export class CdkpipelinesDemoPipelineStack extends Stack {
     // This is where we add the application stages
     pipeline.addStage(
         new CdkpipelinesDemoStage(this, "PreProd", {
-          env: { account: "394535346065", region: "us-west-2" },
+          env: { account, region },
         })
     );
+
+    const prod = new CdkpipelinesDemoStage(this, "Prod", {
+      env: { account, region },
+    });
+
+    pipeline.addStage(prod, {
+      pre: [ new ManualApprovalStep("PromoteToProd") ],
+    });
   }
 }
